d8b5948e140369b6fbb5b895214780e97319f67d
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-05-12 15:18:10 +0530

    Added: Project Section in Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Technology from "./components/Technology"
+import Experience from "./components/Experience";
 const App = () => {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
@@ -14,6 +15,7 @@ const App = () => {
         <Hero />
         <About/>
         <Technology />
+        <Experience />
       </div>
     </div>
   )
