bb49792f159e5fbc314b3002e292224910de0430
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-05-12 16:17:09 +0530

    Made some final changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from "./components/About";
 import Technology from "./components/Technology"
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 const App = () => {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
@@ -18,6 +19,7 @@ const App = () => {
         <Technology />
         <Experience />
         <Projects/>
+        <Contact />
       </div>
     </div>
   )
--- src/constants/index.js
+++ src/constants/index.js
@@ -3,74 +3,57 @@ import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 3 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 3 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
 export const EXPERIENCES = [
   {
     year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    role: "Software Developer",
+    company: "SOTI Inc.",
+    description: `Developed, maintained, and scaled a robust UI automation framework utilizing C#, enhancing testing efficiency and 
+reliability. 
+Working on design and implementation of a comprehensive Automation framework for UI starting from the ground up.
+Leveraged Playwright Web Driver for web automation and harnessed Appium for mobile automation, all within the .NET 
+framework. `,
+    technologies: ["Playwright", "Appium", ".NET", "C#"],
   },
+
   {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-  },
-  {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+    year: "Feb 2022 - july 2022",
+    role: "Software Engineer Intern",
+    company: "Lenovo",
+    description: `Contributed to the development of web applications using JavaScript, Angular.js, and Node.js. Refactored unit tests for microservices, achieving 90% threshold coverage using code coverage tools. . Worked closely with product managers to prioritize features and enhancements.`,
+    technologies: ["NodeJS", "ExpressJS", "JavaScript"],
   },
 ];
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "ShopKart E-Commerce Website",
     image: project1,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+      "Shop Kart is a front-end e-commerce application built using HTML, CSS, and Vanilla JavaScript. It utilizes the FakeStore API to fetch product data, Axios for making API calls, DOM APIs for manipulation, and Bootstrap for styling and UI components.",
+    technologies: ["HTML", "CSS", "Axios", "Bootstrap"],
   },
   {
-    title: "Task Management App",
+    title: "Codeial Social Media App",
     image: project2,
-    description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+    description:"Codeial is a social networking platform built using Node.js, Express.js, MongoDB, and other web technologies. It allows users to register, create posts, comment on posts, add friends, and engage in various social interactions. A dynamic social media platform for tech enthusiasts. Users can register, connect, and explore a world of knowledge sharing while engaging with captivating posts, comments, and real-time chats",
+    technologies: ["HTML", "EJS", "ExpressJS","NodeJS","MongoDB", "Redis"],
   },
   {
     title: "Portfolio Website",
     image: project3,
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
-  },
-  {
-    title: "Blogging Platform",
-    image: project4,
-    description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
-  },
+    technologies: ["HTML", "CSS", "React", "Tailwind", "Framer-Motion"],
+  }
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "R.K. Puram, New Delhi, 110022 ",
+  phoneNo: "+91 93-###-392-17",
+  email: "abhishek.bisht.21@gmail.com",
 };
